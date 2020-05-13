a594d9eed00aba5b013951fd4c0146e19ded41a4
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2020-05-13 19:55:56 +0200

    asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 package com.company;
+
 
 public class Main {
 
+    Main(){
+
+    }
+
+    private void writeProductsXLM(){
+        // DocumentBuilderFactory
+        // DocumentBuilder
+        // Document
+
+        // Build XML Elements and Text Nodes
+
+        // Set OutputFormat
+
+        // Declare the File
+        // Declare the FileOutputStream
+
+        // XMLSerializer to serialize the XML data with
+        // the specified output format
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    Main productsXML = new Main();
     }
 }
